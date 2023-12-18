ed966d6c60d774c40a48cfc1397d458b7605cca1
eb3831 <eb3831@bs.amalnet.k12.il>
2023-12-18 14:45:37 +0200

    b

--- app/src/main/java/com/example/button4/MainActivity.java
+++ app/src/main/java/com/example/button4/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn);
+
     }
 
     public void clicked(View view) {
