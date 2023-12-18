76a3751f7ef54dff82e428384de4dba1912408ec
eb3831 <eb3831@bs.amalnet.k12.il>
2023-12-18 11:53:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/button4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.button4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int num=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+    }
+
+    public void clicked(View view) {
+        num++;
+        btn.setText("this is click number: "+num);
+        if (num%7==0){
+            btn.setText("BOOM!");
+
+        }
+
+    }
+}
